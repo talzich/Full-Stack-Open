bc3d7c9cdde77a0ecc34b24ccaf269000d95d50e
Tal <talzhtc@gmail.com>
2021-08-18 11:14:59 +0300

    Learning JSX

--- Part_1/src/App.js
+++ Part_1/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b =20
 
-  return(
+const Hello = () => {
+  return (
+  <div>
+    <p>Hello, World!</p>
+  </div>
+  )
+}
+
+const App = () => {
+  return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
     </div>
   )
 }
