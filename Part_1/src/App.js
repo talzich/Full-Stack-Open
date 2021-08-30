40dbd6a01ed8321dc83a7207141de52455dc741c
Tal <talzhtc@gmail.com>
2021-08-30 23:00:19 +0300

    Back to the start of this part to get a better understanding

--- Part_1/src/App.js
+++ Part_1/src/App.js
@@ -1,22 +1,8 @@
 import React from 'react'
-
-const Hello = (props) => {
-  return (
+const App = () => (
   <div>
-    <p>Hello, {props.name}</p>
-    <p>You are {props.age} years old</p>
+    <p>Hello world</p>
   </div>
-  )
-}
-
-const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name = "Tal"/>
-      <Hello age = "27"/>
-    </div>
-  )
-}
+)
 
 export default App
