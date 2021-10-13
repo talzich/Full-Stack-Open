e5b76cc5fcf60a3bd906cd9c397f7afd355cb6ac
Tal <talzhtc@gmail.com>
2021-10-13 20:30:28 +0300

    Using hooks, states and reusable components

--- part_1_exercise/src/App.js
+++ part_1_exercise/src/App.js
@@ -1,63 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course.name}</h1>
-}
 
-const Content = (props) => {
-  console.log(props)
-  return(
-    <>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </>
-  )
-}
-
-const Part = (props) => {
+const Display = (props) => {
   return (
-    <>
-      <p>{props.part.name} {props.part.exercises}</p>
-    </>
+    <div>{props.counter}</div>
   )
 }
 
-const Total = (props) => {
-  console.log(props)
-  const exer1 = props.course.parts[0].exercises;
-  const exer2 = props.course.parts[1].exercises;
-  const exer3 = props.course.parts[2].exercises;
+const Button = (props) => {
   return(
-    <p>Number of exercises {exer1+exer2+exer3}</p>
+    <button onClick={props.onClick}>{props.text}</button>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  } 
-  
+  const [counter, setCounter] = useState(0)
+
+  const decrement = () => setCounter(counter - 1)
+  const increment = () => setCounter(counter + 1)
+  const reset = () => setCounter(0)
+
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Display counter={counter}/>
+      <Button onClick={decrement} text='minus'/>
+      <Button onClick={reset} text='zero'/>
+      <Button onClick={increment} text='plus'/>
     </div>
   )
 }
